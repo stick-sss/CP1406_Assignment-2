a03b06af3ea78330ea7a73cfd8824225928cd423
stick-sss <75612854+stick-sss@users.noreply.github.com>
2021-06-04 22:06:47 +0800

    Add files via upload

---
+++ index/information.php
@@ -0,0 +1,190 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>information</title>
+<style type="text/css">
+
+.fk{width: 79.1%;height: 120px;background-color:#2c2e38;margin-left:191px;position:absolute}
+.logo{position:absolute;width:220px;height:200px;margin-left:230px;}
+body{background: url(../bakery/bkg.png);background-size:cover;}
+h1{font-size:5em;position:absolute;margin-left:750px;margin-top:170px;color:#ff8c1a}
+
+.navigation1{width:200px;height:100px;}	
+#container1{position: absolute;margin-left:475px;margin-top:10px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text1{position: absolute;top: 55%;left: 50%;transform: translate(-50%, -50%);font-size: 21px;color:balck;font-weight:bold;text-align:center}
+#container1:hover{transform: scale(1.2);}
+
+.navigation2{width:200px;height:100px;}
+#container2{position: absolute;margin-left:695px;margin-top:10px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text2{position: absolute;top: 55%;left:50% ;transform: translate(-50%, -50%);font-size: 21px;color:balck;font-weight:bold;text-align:center}
+#container2:hover{transform: scale(1.2);}
+
+.navigation3{width:200px;height:100px;}
+#container3{position: absolute;margin-left:915px;margin-top:10px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text3{position: absolute;top: 60%;left: 50%;transform: translate(-50%, -50%);font-size: 21px;color:balck;font-weight:bold;text-align:center}
+#container3:hover{transform: scale(1.2);}
+
+.navigation4{width:200px;height:100px;}
+#container4{position: absolute;margin-left:1135px;margin-top:10px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text4{position: absolute;top: 55%;left: 50%;transform: translate(-50%, -50%);font-size: 21px;color:balck;font-weight:bold;text-align:center}
+#container4:hover{transform: scale(1.2);}
+
+.navigation5{width:200px;height:100px;}
+#container5{position: absolute;margin-left:1355px;margin-top:10px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text5{position: absolute;top: 55%;left: 50%;transform: translate(-50%, -50%);font-size: 21px;color:balck;font-weight:bold;text-align:center}
+#container5:hover{transform: scale(1.2);}
+
+a{text-decoration:none;color:white}
+
+.footer {
+	width: 79.1%;
+	text-align: center;
+	font-size: 0.7em;
+	background-color: #2c2e38;
+	color: #d7ceb2;
+	font: "Helvetica", Arial, sans-serif;
+	margin-top:913px;
+	margin-left:192px;
+	position:absolute;}
+	
+
+.txt2{position:absolute;margin-left:700px;margin-top:650px;font-size:1.5em}
+.txt3{position:absolute;margin-left:700px;margin-top:720px;font-size:1.5em}
+.txt4{position:absolute;margin-left:700px;margin-top:790px;font-size:1.5em}
+	
+.exe1{position:absolute;margin-left:900px;margin-top:650px;font-size:1.5em}
+.exe2{position:absolute;margin-left:900px;margin-top:720px;font-size:1.5em}
+.exe3{position:absolute;margin-left:900px;margin-top:790px;font-size:1.5em}
+
+.txt5{position:absolute;margin-left:700px;margin-top:370px;font-size:1.5em}
+.txt6{position:absolute;margin-left:700px;margin-top:440px;font-size:1.5em}
+.txt7{position:absolute;margin-left:700px;margin-top:510px;font-size:1.5em}
+.txt8{position:absolute;margin-left:700px;margin-top:580px;font-size:1.5em}
+
+
+
+
+
+
+
+
+
+
+@media only screen and (max-width: 780px){
+.logo{position:absolute;width:60px;height:60px;margin-left:90px;margin-top:10px}
+
+.navigation1{width:80px;height:50px;}	
+#container1{position: absolute;margin-left:80px;margin-top:80px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text1{position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);font-size: 6px;color:balck;font-weight:bold;}
+#container1:hover{transform: scale(1.2);}
+
+.navigation2{width:80px;height:50px;}
+#container2{position: absolute;margin-left:210px;margin-top:80px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text2{position: absolute;top: 50%;left:47.5% ;transform: translate(-50%, -50%);font-size: 6px;color:balck;font-weight:bold;text-align:center}
+#container2:hover{transform: scale(1.2);}
+
+.navigation3{width:80px;height:50px;}
+#container3{position: absolute;margin-left:80px;margin-top:138px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text3{position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);font-size: 6px;color:balck;font-weight:bold}
+#container3:hover{transform: scale(1.2);}
+
+.navigation4{width:80px;height:50px;}
+#container4{position: absolute;margin-left:210px;margin-top:138px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text4{position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);font-size: 6px;color:balck;font-weight:bold;text-align:center}
+#container4:hover{transform: scale(1.2);}
+
+.navigation5{width:80px;height:50px;}
+#container5{position: absolute;margin-left:210px;margin-top:20px;transition-property: all;transition-duration: 0.5s;transition-timing-function: ease;transition-delay: 0s;}
+#text5{position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);font-size: 6px;color:balck;font-weight:bold;text-align:center}
+#container5:hover{transform: scale(1.2);}
+
+.fk{width: 79%;height: 198px;background-color:#2c2e38;margin-left:32px;position:absolute}
+
+h1{font-size:2.4em;position:absolute;margin-left:70px;margin-top:220px;color:#ff8c1a}
+
+.footer {
+	position:absolute;
+	width: 79.1%;
+	text-align: center;
+	font-size: 6px;
+	background-color: #2c2e38;
+	color: #d7ceb2;
+	font: "Helvetica", Arial, sans-serif;
+	margin-top:500px;
+	margin-left:32px;
+	transform: scale(0.25);
+}
+
+.txt2{position:absolute;margin-left:35px;margin-top:420px;font-size:6px}
+.txt3{position:absolute;margin-left:35px;margin-top:450px;font-size:6px}
+.txt4{position:absolute;margin-left:35px;margin-top:480px;font-size:6px}
+	
+.exe1{position:absolute;margin-left:120px;margin-top:420px;font-size:6px}
+.exe2{position:absolute;margin-left:120px;margin-top:450px;font-size:6px}
+.exe3{position:absolute;margin-left:120px;margin-top:480px;font-size:6px}
+
+.txt5{position:absolute;margin-left:35px;margin-top:300px;font-size:6px}
+.txt6{position:absolute;margin-left:35px;margin-top:330px;font-size:6px}
+.txt7{position:absolute;margin-left:35px;margin-top:360px;font-size:6px}
+.txt8{position:absolute;margin-left:35px;margin-top:390px;font-size:6px}
+
+}
+</style>
+</head>
+<body>
+<h1>Information</h1>
+<div class="fk"></div>
+<img class="logo" src="../bakery/logo.png">
+
+<div id="container1">
+	<a href="../index.html">
+	<img class="navigation1" src="../bakery/Bakery.png" alt="Home page" >
+	<div id="text1">Home page</div>
+	</a>
+</div>
+<div id="container2">
+	<a href="Weekly Specials.html">
+	<img class="navigation2" src="../bakery/Bakery.png" alt="Weekly Specials">
+	<div id="text2">Weekly Specials</div>
+	</a>
+</div>
+<div id="container3">
+	<a href="Menu.html">
+	<img class="navigation3" src="../bakery/Bakery.png" alt="Menu">
+	<div id="text3">Menu</div>
+	</a>
+</div>
+<div id="container4">
+	<a href="Specials page.html">
+	<img class="navigation4" src="../bakery/Bakery.png" alt="Specials page">
+	<div id="text4">Specials page</div>
+	</a>
+</div>
+<div id="container5">
+	<a href="About me.html">
+	<img class="navigation5" src="../bakery/Bakery.png" alt="About me">
+	<div id="text5">About me</div>
+	</a>
+</div>
+
+<div class="footer">
+	Contacts: 0455 5555 555</br>
+	Catering: 0433 3333 333</br>
+	Address: 123 Prize Lane, Kirwan, QLD
+</div>
+
+<div class="txt2">Our Contacts:</div>
+<div class="txt3">Our Catering:</div>
+<div class="txt4">Our Address:</div>
+
+<div class="exe1">0455 5555 555</div>
+<div class="exe2">0433 3333 333</div>
+<div class="exe3">123 Prize Lane, Kirwan, QLD</div>
+
+<div class="txt5">Welcome <?php echo $_POST["username"]; ?><br></div>
+<div class="txt6">Your email address is: <?php echo $_POST["email"]; ?><br></div>
+<div class="txt7">Your Product interests is: <?php echo $_POST["proint"]; ?><br></div>
+<div class="txt8">Your birthday is: <?php echo $_POST["day"]; ?> / <?php echo $_POST["month"]; ?></div>
+
+</body>
+</html>
